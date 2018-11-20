4c8125b7f1c21b1aa3c0db305c025aaddc4c49e9
ianporto1 <33235402+ianporto1@users.noreply.github.com>
2018-11-20 14:34:34 -0200

    Bucetao
    bugou os branch

--- app/src/main/java/br/com/saecatalao/sae30/Tarifas.java
+++ app/src/main/java/br/com/saecatalao/sae30/Tarifas.java
@@ -1,7 +1,10 @@
 package br.com.saecatalao.sae30;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Tarifas extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Tarifas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarifas);
+
+        Button voltarHome = (Button) findViewById(R.id.button_tarifas_voltar);
+
+        voltarHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Tarifas.this, home.class);
+                finish();
+            }
+        });
     }
+
 }
