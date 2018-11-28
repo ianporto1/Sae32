330357a22a65f44d05fc59c01fac372dbb703b0d
allanleite <allanleite.97@gmail.com>
2018-11-28 13:22:12 -0200

    bora

--- app/src/main/java/br/com/saecatalao/sae30/EditarDadoCelular.java
+++ app/src/main/java/br/com/saecatalao/sae30/EditarDadoCelular.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import static br.com.saecatalao.sae30.Declaracoes.user1;
 
 public class EditarDadoCelular extends AppCompatActivity {
 
@@ -13,6 +16,10 @@ public class EditarDadoCelular extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_dado_celular);
+
+        TextView nomeView = findViewById(R.id.text_display_celular);
+        String cel = Integer.toString(user1.celular);
+        nomeView.setText(cel);
 
         Button celularVoltar = findViewById(R.id.button_editar_celular_voltar);
 
@@ -38,6 +45,9 @@ public class EditarDadoCelular extends AppCompatActivity {
                         }).setPositiveButton("Sim", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        TextView editCelular = findViewById(R.id.edit_celular);
+                        int editC = Integer.parseInt(editCelular.getText().toString());
+                        user1.setCelular(editC);
                         finish();
                     }
                 });
