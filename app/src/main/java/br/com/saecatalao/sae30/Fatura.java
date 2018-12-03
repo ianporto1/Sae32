e1c40ac6678adb515e524d0a346534d17798058a
ianporto1 <33235402+ianporto1@users.noreply.github.com>
2018-12-03 12:54:31 -0200

    oi galero

--- app/src/main/java/br/com/saecatalao/sae30/Fatura.java
+++ app/src/main/java/br/com/saecatalao/sae30/Fatura.java
@@ -1,10 +1,12 @@
 package br.com.saecatalao.sae30;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Fatura extends AppCompatActivity {
 
@@ -21,5 +23,22 @@ public class Fatura extends AppCompatActivity {
                 finish();
             }
         });
+
+        Button down = findViewById(R.id.button_fatura_gerar);
+
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.joinville.udesc.br/portal/professores/claudinei/materiais/Exemplo_de_Plano_de_Software.docx"));
+                startActivity(intent);
+                alert("Fatura gerada com Sucesso!!!");
+                intent = new Intent(Fatura.this,Fatura.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void alert(String s){
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 }
